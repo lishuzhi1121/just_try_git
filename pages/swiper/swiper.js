75b0a6d61d45798c9cb3731aa7f96841015bd130
tyki <iamityki@163.com>
2018-01-31 21:09:32 +0800

    add page dots

--- pages/swiper/swiper.js
+++ pages/swiper/swiper.js
@@ -2,15 +2,22 @@
 
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    slider: [
+      {picUrl:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg'},
+      {picUrl:'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'},
+      {picUrl:'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'}
     ],
-    indicatorDots: true,
+    // indicatorDots: true,
     autoplay: true,
     interval: 1500,
     duration: 500,
-    current: 0,
+    // current: 0,
+    swiperCurrent: 0,
+  },
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
   }
+  
 })
